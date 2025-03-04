0ca760288af35e93470e98270e20872dd075970f
bishal maity <bishalmaity90@gmail.com>
2025-03-04 19:31:29 +0530

    added_port_from_env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import { WebSocketServer } from "ws";
 import { GameManager } from "./core/GameManager";
+const PORT = Number(process.env.PORT) || 8080;
 
-const ws=new WebSocketServer({port:8080});
-
+const ws = new WebSocketServer({ port: PORT, host: "0.0.0.0" });
 const  gameManager=new GameManager();
 ws.on('connection',(ws)=>{
     gameManager.addUser(ws);
