c1bdebd1b898c7019fd0bc683f3c410e75376b38
bishal maity <bishalmaity90@gmail.com>
2025-03-05 11:40:42 +0530

    still_build_errors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const PORT = Number(process.env.PORT) || 8080;
 
 const ws = new WebSocketServer({ port: PORT, host: "0.0.0.0" });
 const  gameManager=new GameManager();
-ws.on('connection',(ws)=>{
+ws.on('connection',(ws:any)=>{
     gameManager.addUser(ws);
     ws.on('close',()=>gameManager.removeUser(ws));
 })
