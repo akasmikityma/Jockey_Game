858b5e44dc4ba032c00994cdb84f68e936853740
bishal maity <bishalmaity90@gmail.com>
2025-04-21 08:25:10 +0530

    Reconnection-2

--- frontend/src/store/useSocketHook.ts
+++ frontend/src/store/useSocketHook.ts
@@ -29,8 +29,8 @@
 import { useEffect, useState } from "react";
 import { Socket_ME } from "./atoms";
 import { useRecoilState } from "recoil";
-// const WS_URL='https://jockey-game-1.onrender.com'
-const WS_URL = 'ws://localhost:8080';
+const WS_URL='https://jockey-game-1.onrender.com'
+// const WS_URL = 'ws://localhost:8080';
 
 export const useSocketHook = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
